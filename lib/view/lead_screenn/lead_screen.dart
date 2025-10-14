93ec181ad54ac06858b1eaa94253a38262a65bce
Parth1801 <bhoomiqf@gmail.com>
2025-10-14 16:55:10 +0530

    lead capitlize 14-10-2025

--- lib/view/lead_screenn/lead_screen.dart
+++ lib/view/lead_screenn/lead_screen.dart
@@ -265,33 +265,33 @@ class LeadScreenState extends State<LeadScreen> {
                                                                   getDynamicSizedBox(
                                                                     width: 1.w,
                                                                   ),
-                                                                  SizedBox(
-                                                                    width: 4.h,
-                                                                    height: 4.h,
-                                                                    child: IconButton(
-                                                                      padding:
-                                                                          EdgeInsets
-                                                                              .zero,
-                                                                      icon: const Icon(
-                                                                        Icons
-                                                                            .delete,
-                                                                        color:
-                                                                            red,
-                                                                      ),
-                                                                      onPressed: () {
-                                                                        // ctr.openDeleteBottomSheet(
-                                                                        //   context:
-                                                                        //       context,
-                                                                        // );
-                                                                        // ctr.updateMeetings(
-                                                                        //   context,
-                                                                        // );
-                                                                      },
-                                                                    ),
-                                                                  ),
-                                                                  getDynamicSizedBox(
-                                                                    width: 1.w,
-                                                                  ),
+                                                                  // SizedBox(
+                                                                  //   width: 4.h,
+                                                                  //   height: 4.h,
+                                                                  //   child: IconButton(
+                                                                  //     padding:
+                                                                  //         EdgeInsets
+                                                                  //             .zero,
+                                                                  //     icon: const Icon(
+                                                                  //       Icons
+                                                                  //           .delete,
+                                                                  //       color:
+                                                                  //           red,
+                                                                  //     ),
+                                                                  //     onPressed: () {
+                                                                  //       // ctr.openDeleteBottomSheet(
+                                                                  //       //   context:
+                                                                  //       //       context,
+                                                                  //       // );
+                                                                  //       // ctr.updateMeetings(
+                                                                  //       //   context,
+                                                                  //       // );
+                                                                  //     },
+                                                                  //   ),
+                                                                  // ),
+                                                                  // getDynamicSizedBox(
+                                                                  //   width: 1.w,
+                                                                  // ),
                                                                   // SizedBox(
                                                                   //   width: 1.w,
                                                                   // ),
